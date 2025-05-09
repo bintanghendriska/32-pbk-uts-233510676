3ee90ccfab98889774f84bc1a5f608cc1c2437cc
BINTANG HENDRISKA V <bintanghendriska39@gmail.com>
2025-05-09 09:57:09 +0700

    membuat form

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from 'vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+      <form @submit.prevent="addTodo" class="todo-form">
+        <input v-model="newTodo" type="text" placeholder="Tambahkan kegiatan..." class="todo-input">
+        <button type="submit" class="todo-button">Tambah</button>
+      </form>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
