4fe5933181f7bb03ffab869a0d6efd67e8c2576b
BINTANG HENDRISKA V <bintanghendriska39@gmail.com>
2025-05-09 10:04:27 +0700

    menambahkan add dan delete

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <script setup>
 import { ref, computed } from 'vue'
 
+const newTodo = ref('')
+const todos = ref([])
+
+
+const addTodo = () => {
+  if (newTodo.value.trim()) {
+    todos.value.push({ text: newTodo.value.trim(), completed: false })
+    newTodo.value = ''
+  }
+}
+
+const deleteTodo = (todoToDelete) => {
+  const index = todos.value.indexOf(todoToDelete)
+  if (index !== -1) {
+    todos.value.splice(index, 1)
+  }
+}
+
+const clearCompleted = () => {
+  todos.value = todos.value.filter(todo => !todo.completed)
+}
 </script>
 
 <template>
@@ -8,6 +29,28 @@ import { ref, computed } from 'vue'
         <input v-model="newTodo" type="text" placeholder="Tambahkan kegiatan..." class="todo-input">
         <button type="submit" class="todo-button">Tambah</button>
       </form>
+
+      <div class="filter-section">
+        <label>
+          <input type="radio" value="all" v-model="filter"> Semua
+        </label>
+        <label>
+          <input type="radio" value="incomplete" v-model="filter"> Belum Selesai
+        </label>
+        <label>
+          <input type="radio" value="complete" v-model="filter"> Selesai
+        </label>
+      </div>
+
+      <ul class="todo-list">
+        <li v-for="(todo, index) in filteredTodos" :key="index" class="todo-item">
+          <div class="todo-text-wrapper">
+            <input type="checkbox" v-model="todo.completed">
+            <span :class="{'completed': todo.completed}" class="todo-text">{{ todo.text }}</span>
+          </div>
+          <button @click="deleteTodo(todo)" class="delete-button">Hapus</button>
+        </li>
+      </ul>
 </template>
 
 <style scoped>
